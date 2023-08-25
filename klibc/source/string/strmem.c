4ac1624c77a0b6074d38a1a5b0ddd371d0a0b185
Emil <emilkarpawitz@outlook.de>
2023-08-26 01:33:18 +0200

    Refactor `klibc` library and add string manipulations functions
    The build process for `klibc` library was refactored by removing a redundant inclusion of `stdbool.h`. Also, separate source files for string manipulations (`strmem.c` and `misc.c`) were introduced. They provide functions for copying, concatenating strings as well as more specific use-cases (e.g., changing string case, checking if string is numeric). These changes are oriented towards improving code readability, maintaining project structure, and extending library functionality.

---
+++ klibc/source/string/strmem.c
@@ -0,0 +1,52 @@
+//
+// Created by Emil Glenn Karpawitz on 8/26/23.
+//
+#include <string.h>
+
+char *strcpy(char *dest, const char *src)
+{
+	return (char *)memcpy(dest, src, strlen(src) + 1);
+}
+
+char *strncpy(char *dest, const char *src, size_t count)
+{
+	size_t len = strlen(src);
+	if (len > count)
+		len = count;
+	memcpy(dest, src, len);
+	memset(dest + len, 0, count - len);
+	return dest;
+}
+
+char *strlcpy(char *dest, const char *src, size_t count)
+{
+	size_t len = strlen(src);
+	if (len > count - 1)
+		len = count - 1;
+	memcpy(dest, src, len);
+	dest[len] = 0;
+	return dest;
+}
+
+char *strcat(char *dest, const char *src)
+{
+	strcpy(dest + strlen(dest), src);
+	return dest;
+}
+
+char *strncat(char *dest, const char *src, size_t count)
+{
+	strncpy(dest + strlen(dest), src, count);
+	return dest;
+}
+
+// TODO: add malloc
+char *strdup(const char *src)
+{
+//	size_t len = strlen(src);
+//	char *dest = malloc(len + 1);
+
+//	memcpy(dest, src, len + 1);
+//	return dest;
+	return NULL;
+}

66de5c8e31a9cba46be97855d4a4690f712ea2ab
Robin Alexander Plate <robin@peerstudios.net>
2023-09-01 13:03:40 +0200

    Update Runtime.c to handle runtime errors
    This update introduces two significant changes in Runtime.c. Firstly, we've added more static volatile structs to handle different types of system requests. We now have specific requests for limine kernel files, memory maps, HHDM, and kernel addresses. This should help in handling different types of system requests in a more structured manner.
    
    Secondly, we've introduced error handling in the StartupRoutine. If the MemoryInit or HeapInit functions fail, they will trigger a 'QUICK_CHECK' which will then lead to system halt and disable interrupts to avoid any further complications. A reminder has been added to print out the respective error that caused the halt on the serial line in future updates.
    
    These changes should improve the robustness of the startup routine and aid in diagnosing startup issues in the future.

--- Kernel/RuntimeLib/Runtime.c
+++ Kernel/RuntimeLib/Runtime.c
@@ -9,16 +9,30 @@
 #include <X64/GDT.h>
 #include <X64/IDT.h>
 
-STATIC KERNEL_PARAMETERS                          g_Parameters;
+STATIC KERNEL_PARAMETERS g_Parameters;
 
-STATIC volatile struct limine_kernel_file_request g_KernelFileRequest = { .id = LIMINE_KERNEL_FILE_REQUEST, .revision = 0 };
-STATIC volatile struct limine_memmap_request      g_MemoryRequest     = { .id = LIMINE_MEMMAP_REQUEST, .revision = 0 };
+STATIC volatile struct limine_kernel_file_request
+                                                     g_KernelFileRequest
+                                                                            = { .id = LIMINE_KERNEL_FILE_REQUEST, .revision = 0 };
+
+STATIC volatile struct limine_memmap_request         g_MemoryRequest
+                                                                            = { .id = LIMINE_MEMMAP_REQUEST, .revision = 0 };
+
+STATIC volatile struct limine_hhdm_request           g_HHDMRequest
+                                                                            = { .id = LIMINE_HHDM_REQUEST, .revision = 0 };
+
+STATIC volatile struct limine_kernel_address_request g_KernelAddressRequest
+                                                                            = { .id = LIMINE_KERNEL_ADDRESS_REQUEST, .revision = 0 };
+
+#define QUICK_CHECK(x) if (FAILED(x)) { goto Exit; }
 
 VOID
 StartupRoutine (
     VOID
     )
 {
+    STATUS Status;
+
     // TODO: split this into multiple command line variables
     g_Parameters.CommandLineSize = 1;
     g_Parameters.CommandLine     = (CHAR8**)g_KernelFileRequest.response->kernel_file->cmdline;
@@ -34,12 +48,64 @@ StartupRoutine (
     //
     GDTInit ();
     IDTInit ();
-
-    MemoryInit (&g_Parameters);
-    HeapInit ();
+    
+    QUICK_CHECK (MemoryInit(&g_Parameters));
+    QUICK_CHECK (HeapInit());
+    
+    //
+    // we have to set up our Virtual Address Space, so we don't rely on the bootloader
+    // anymore.
+    //
+    QUICK_CHECK (CreateVirtualAddressSpace(&g_Parameters.VirtualAddressSpace));
+    
+    UINTN Size = 0;
+    
+    for (UINTN i = 0; i < g_Parameters.BootMemoryMapSize; i++) {
+        BOOT_MEMORY_DESCRIPTOR* Descriptor = g_Parameters.BootMemoryMap[i];
+        
+        if (Descriptor->Type != MEMMAP_RESERVED) {
+            Size += Descriptor->Length;
+        }
+    }
+    
+    Size = ALIGN_UP (Size, SIZE_1GB);
+    
+    UINTN Pages = Size / SIZE_1GB;
+    
+    QUICK_CHECK (MapVirtualAddress(g_Parameters.VirtualAddressSpace,
+        0x0000000000000000,
+        Pages,
+        MEMORY_ATTRIBUTE_READ | MEMORY_ATTRIBUTE_WRITE | MEMORY_ATTRIBUTE_EXEC,
+        MEMORY_TYPE_1GB_PAGE,
+        0x0000000000001000));
+    QUICK_CHECK (MapVirtualAddress(g_Parameters.VirtualAddressSpace,
+        0x0000000000000000,
+        Pages,
+        MEMORY_ATTRIBUTE_READ | MEMORY_ATTRIBUTE_WRITE | MEMORY_ATTRIBUTE_EXEC,
+        MEMORY_TYPE_1GB_PAGE,
+        g_HHDMRequest.response->offset));
+    QUICK_CHECK (MapVirtualAddress(g_Parameters.VirtualAddressSpace,
+        g_KernelAddressRequest.response->physical_base,
+        Pages,
+        MEMORY_ATTRIBUTE_READ | MEMORY_ATTRIBUTE_WRITE | MEMORY_ATTRIBUTE_EXEC,
+        MEMORY_TYPE_1GB_PAGE,
+        g_KernelAddressRequest.response->virtual_base));
+    
+    QUICK_CHECK (ActivateVirtualAddressSpace(g_Parameters.VirtualAddressSpace));
     
     //
     // Enter the Kernel
     //
     KernelMain (&g_Parameters);
+
+Exit:
+    //
+    // We fucked up.
+    // TODO: Print to serial that we fucked up and why..
+    //
+    DisableInterrupts();
+    
+    while (TRUE) {
+        __asm__ volatile ("hlt");
+    }
 }

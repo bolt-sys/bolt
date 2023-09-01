77da345cb9cacd464c74360d843792dfb1d7d0d8
Robin Alexander Plate <robin@peerstudios.net>
2023-09-01 13:12:24 +0200

    oops

--- Kernel/RuntimeLib/Runtime.c
+++ Kernel/RuntimeLib/Runtime.c
@@ -9,20 +9,12 @@
 #include <X64/GDT.h>
 #include <X64/IDT.h>
 
-STATIC KERNEL_PARAMETERS g_Parameters;
+STATIC KERNEL_PARAMETERS                             g_Parameters;
 
-STATIC volatile struct limine_kernel_file_request
-                                                     g_KernelFileRequest
-                                                                            = { .id = LIMINE_KERNEL_FILE_REQUEST, .revision = 0 };
-
-STATIC volatile struct limine_memmap_request         g_MemoryRequest
-                                                                            = { .id = LIMINE_MEMMAP_REQUEST, .revision = 0 };
-
-STATIC volatile struct limine_hhdm_request           g_HHDMRequest
-                                                                            = { .id = LIMINE_HHDM_REQUEST, .revision = 0 };
-
-STATIC volatile struct limine_kernel_address_request g_KernelAddressRequest
-                                                                            = { .id = LIMINE_KERNEL_ADDRESS_REQUEST, .revision = 0 };
+STATIC volatile struct limine_kernel_file_request    g_KernelFileRequest    = { .id = LIMINE_KERNEL_FILE_REQUEST, .revision = 0 };
+STATIC volatile struct limine_memmap_request         g_MemoryRequest        = { .id = LIMINE_MEMMAP_REQUEST, .revision = 0 };
+STATIC volatile struct limine_hhdm_request           g_HHDMRequest          = { .id = LIMINE_HHDM_REQUEST, .revision = 0 };
+STATIC volatile struct limine_kernel_address_request g_KernelAddressRequest = { .id = LIMINE_KERNEL_ADDRESS_REQUEST, .revision = 0 };
 
 #define QUICK_CHECK(x) if (FAILED(x)) { goto Exit; }
 
@@ -48,51 +40,10 @@ StartupRoutine (
     //
     GDTInit ();
     IDTInit ();
-    
-    QUICK_CHECK (MemoryInit(&g_Parameters));
-    QUICK_CHECK (HeapInit());
-    
-    //
-    // we have to set up our Virtual Address Space, so we don't rely on the bootloader
-    // anymore.
-    //
-    QUICK_CHECK (CreateVirtualAddressSpace(&g_Parameters.VirtualAddressSpace));
-    
-    UINTN Size = 0;
-    
-    for (UINTN i = 0; i < g_Parameters.BootMemoryMapSize; i++) {
-        BOOT_MEMORY_DESCRIPTOR* Descriptor = g_Parameters.BootMemoryMap[i];
-        
-        if (Descriptor->Type != MEMMAP_RESERVED) {
-            Size += Descriptor->Length;
-        }
-    }
-    
-    Size = ALIGN_UP (Size, SIZE_1GB);
-    
-    UINTN Pages = Size / SIZE_1GB;
-    
-    QUICK_CHECK (MapVirtualAddress(g_Parameters.VirtualAddressSpace,
-        0x0000000000000000,
-        Pages,
-        MEMORY_ATTRIBUTE_READ | MEMORY_ATTRIBUTE_WRITE | MEMORY_ATTRIBUTE_EXEC,
-        MEMORY_TYPE_1GB_PAGE,
-        0x0000000000001000));
-    QUICK_CHECK (MapVirtualAddress(g_Parameters.VirtualAddressSpace,
-        0x0000000000000000,
-        Pages,
-        MEMORY_ATTRIBUTE_READ | MEMORY_ATTRIBUTE_WRITE | MEMORY_ATTRIBUTE_EXEC,
-        MEMORY_TYPE_1GB_PAGE,
-        g_HHDMRequest.response->offset));
-    QUICK_CHECK (MapVirtualAddress(g_Parameters.VirtualAddressSpace,
-        g_KernelAddressRequest.response->physical_base,
-        Pages,
-        MEMORY_ATTRIBUTE_READ | MEMORY_ATTRIBUTE_WRITE | MEMORY_ATTRIBUTE_EXEC,
-        MEMORY_TYPE_1GB_PAGE,
-        g_KernelAddressRequest.response->virtual_base));
-    
-    QUICK_CHECK (ActivateVirtualAddressSpace(g_Parameters.VirtualAddressSpace));
-    
+
+    QUICK_CHECK (MemoryInit (&g_Parameters));
+    QUICK_CHECK (HeapInit ());
+
     //
     // Enter the Kernel
     //
@@ -103,8 +54,8 @@ Exit:
     // We fucked up.
     // TODO: Print to serial that we fucked up and why..
     //
-    DisableInterrupts();
-    
+    DisableInterrupts ();
+
     while (TRUE) {
         __asm__ volatile ("hlt");
     }

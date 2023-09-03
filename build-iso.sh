163521640d5c0a3cf34bbd43a7c785d2cab9e0d2
Robin Alexander Plate <robin@peerstudios.net>
2023-09-03 04:04:18 +0200

    Adjust paths

--- build-iso.sh
+++ build-iso.sh
@@ -8,16 +8,27 @@ function build-iso() {
   rm -rf build/iso_root
   mkdir -p build/iso_root
 
-  make -C build/cmake/_deps/limine-src -j"$(nproc)"
+  if [ -d build/cmake/_deps/limine-src ]; then
+    echo "-----"
+    echo "- Please move build/cmake/ to build/"
+    echo "-----"
+    BUILDC_DIR="cmake/"
+
+    make -C build/cmake/_deps/limine-src -j"$(nproc)"
+  else
+    BUILDC_DIR="."
+
+    make -C build/_deps/limine-src -j"$(nproc)"
+  fi
 
   pushd build
     #ninja -C cmake
     ls ../
-    cp -v cmake/Kernel/Kernel ../limine.cfg cmake/_deps/limine-src/limine-bios.sys cmake/_deps/limine-src/limine-bios-cd.bin cmake/_deps/limine-src/limine-uefi-cd.bin iso_root/
+    cp -v $BUILDC_DIR/Kernel/Kernel ../limine.cfg $BUILDC_DIR/_deps/limine-src/limine-bios.sys $BUILDC_DIR/_deps/limine-src/limine-bios-cd.bin $BUILDC_DIR/_deps/limine-src/limine-uefi-cd.bin iso_root/
 
     mkdir -p iso_root/EFI/BOOT
-    cp -v cmake/_deps/limine-src/BOOTX64.EFI iso_root/EFI/BOOT/
-    cp -v cmake/_deps/limine-src/BOOTIA32.EFI iso_root/EFI/BOOT/
+    cp -v $BUILDC_DIR/_deps/limine-src/BOOTX64.EFI iso_root/EFI/BOOT/
+    cp -v $BUILDC_DIR/_deps/limine-src/BOOTIA32.EFI iso_root/EFI/BOOT/
 
     xorriso -as mkisofs -b limine-bios-cd.bin \
       -no-emul-boot -boot-load-size 4 -boot-info-table \
@@ -25,7 +36,7 @@ function build-iso() {
       -efi-boot-part --efi-boot-image --protective-msdos-label \
       iso_root -o "$IMAGE_NAME.iso"
 
-    cmake/_deps/limine-src/limine bios-install "$IMAGE_NAME.iso"
+    $BUILDC_DIR/_deps/limine-src/limine bios-install "$IMAGE_NAME.iso"
     rm -rf iso_root
   popd
 }
